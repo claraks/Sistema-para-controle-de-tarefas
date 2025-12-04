2b95aa28b282cf1acfb9ed232e6668a364164b6a
claraks <claraklassmann@gmail.com>
2025-12-04 00:45:17 -0300

    Add files via upload

---
+++ cadastro de tarefas dw/cadastro.js
@@ -0,0 +1,45 @@
+document.getElementById("formTarefa").addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const prioridade = document.getElementById("prioridade").value;
+    const descricao = document.getElementById("descricao").value;
+    const local = document.getElementById("local").value;
+
+    const recursos = document.getElementById("recursos").value
+        .split("\n")
+        .map(r => r.trim())
+        .filter(r => r.length > 0);
+
+    const dataLimite = document.getElementById("dataLimite").value;
+    const matricula = Number(document.getElementById("matricula").value);
+
+    const tarefa = {
+        prioridade,
+        descricao,
+        local,
+        recursosNecessarios: recursos,
+        dataLimite: dataLimite.replace("T", " ") + ":00",
+        matricula,
+        concluida: false,
+        tipo: "tarefa"
+    };
+
+    try {
+        const resp = await fetch("http://159.65.228.63/tarefas", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(tarefa)
+        });
+
+        if (resp.ok) {  
+            alert("Tarefa cadastrada com sucesso!");
+            window.location.href = "index.html";
+        } else {
+            alert("Erro ao cadastrar tarefa!");
+        }
+
+    } catch (erro) {
+        alert("Erro ao conectar à API.");
+        console.error(erro);
+    }
+});
